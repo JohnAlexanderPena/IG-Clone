2515180b6fc1e7da382f012becaa799e013218fe
jpena91@gmail.com <jpena91@gmail.com>
2021-03-15 09:04:33 -0400

    quick login test

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -20,7 +20,12 @@ export default function Login() {
 
   return (
     <div className="container flex mx-auto max-w-screen-md items-center h-screen">
-      <p>I am the login page</p>
+      <div className="flex w-3/5">
+        <img src="/images/iphone-with-profile.jpg" alt="ig-default" />
+      </div>
+      <div className="flex flex-col w-2/5">
+        <p>I am the login page</p>
+      </div>
     </div>
   );
 }
