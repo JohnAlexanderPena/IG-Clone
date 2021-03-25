4a142345a6cdcd4c131f608b0eaf22df9c87dd63
jpena91@gmail.com <jpena91@gmail.com>
2021-03-25 02:13:35 -0400

    add default timeline, test header

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,5 +1,6 @@
 import Skeleton from 'react-loading-skeleton';
 import usePhotos from '../hooks/use-photos';
+import Post from './Post';
 export default function Timeline() {
   const { photos } = usePhotos();
 
@@ -9,7 +10,9 @@ export default function Timeline() {
       {!photos ? (
         <Skeleton count={4} width={640} height={500} className="mb-5" />
       ) : photos?.length > 0 ? (
-        photos.map((content) => <p key={content.docId}>{content.imageSrc}</p>)
+        photos.map((content) => (
+          <Post key={content.docId} content={content}></Post>
+        ))
       ) : (
         <p className="text-center text-2xl">
           Follow people to see what they're doing
